0e88a65adcfe3f6cbe71d736bae770f41d3b105c
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-08-24 16:13:44 +0200

    chore: resolve enum.entries usage if external enum extended from enum base class.

--- compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/converter/DeclarationsConverter.kt
+++ compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/converter/DeclarationsConverter.kt
@@ -607,7 +607,7 @@ class DeclarationsConverter(
                             modifiers.hasExpect()
                         )
 
-                        if (!modifiers.hasExternal()) {
+                        if (superTypeRefs.any { it.isEnum }) {
                             generateEntriesGetter(
                                 baseModuleData,
                                 context.packageFqName,
