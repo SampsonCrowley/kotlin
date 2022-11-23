3c12df8db5911473bbc01483eebdf6fdf930ecfa
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-11-23 16:29:54 +0100

    ~ std def

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/FirScriptConfigurationServiceImpl.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/FirScriptConfigurationServiceImpl.kt
@@ -6,12 +6,33 @@
 package org.jetbrains.kotlin.scripting.compiler.plugin
 
 import org.jetbrains.kotlin.fir.builder.FirScriptConfiguratorService
+import org.jetbrains.kotlin.fir.declarations.FirDeclarationOrigin
+import org.jetbrains.kotlin.fir.declarations.builder.buildValueParameter
 import org.jetbrains.kotlin.fir.extensions.FirExtensionSessionComponent
+import org.jetbrains.kotlin.fir.moduleData
+import org.jetbrains.kotlin.fir.symbols.impl.FirValueParameterSymbol
+import org.jetbrains.kotlin.fir.types.builder.buildResolvedTypeRef
+import org.jetbrains.kotlin.fir.types.coneType
+import org.jetbrains.kotlin.name.Name
 
 fun firScriptConfigurationServiceFactory(): FirExtensionSessionComponent.Factory {
     return FirExtensionSessionComponent.Factory { session ->
+        val stringArrayTypeRef = buildResolvedTypeRef {
+            type = session.builtinTypes.stringType.coneType
+        }
         FirScriptConfiguratorService(session) {
-            println(1)
+            it.valueParameters.add(
+                buildValueParameter {
+                    moduleData = session.moduleData
+                    origin = FirDeclarationOrigin.ScriptCustomization
+                    returnTypeRef = stringArrayTypeRef
+                    this.name = Name.identifier("args")
+                    this.symbol = FirValueParameterSymbol(this.name)
+                    isCrossinline = false
+                    isNoinline = false
+                    isVararg = true
+                }
+            )
         }
     }
 }
