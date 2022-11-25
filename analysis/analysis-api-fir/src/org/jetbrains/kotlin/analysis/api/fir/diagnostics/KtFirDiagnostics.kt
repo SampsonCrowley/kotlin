790142beb8ba164d463da1b6064a659c4d9c9173
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-11-25 15:30:13 +0100

    K2: add checker detecting protected Java field shadowed by a property

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnostics.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnostics.kt
@@ -3164,6 +3164,12 @@ sealed class KtFirDiagnostic<PSI : PsiElement> : KtDiagnosticWithPsi<PSI> {
         override val diagnosticClass get() = JavaSamInterfaceConstructorReference::class
     }
 
+    abstract class JavaShadowedProtectedFieldReference : KtFirDiagnostic<PsiElement>() {
+        override val diagnosticClass get() = JavaShadowedProtectedFieldReference::class
+        abstract val containerClass: ClassId
+        abstract val shadowingClass: ClassId
+    }
+
     abstract class Syntax : KtFirDiagnostic<PsiElement>() {
         override val diagnosticClass get() = Syntax::class
     }
