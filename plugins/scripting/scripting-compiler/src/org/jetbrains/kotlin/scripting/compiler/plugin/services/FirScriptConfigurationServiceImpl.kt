823594e301b418231ff947f45f8f272a9392b22d
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-11-29 14:59:06 +0100

    ~ conf: implicit receivers & provided props

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/services/FirScriptConfigurationServiceImpl.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/services/FirScriptConfigurationServiceImpl.kt
@@ -13,20 +13,21 @@ import org.jetbrains.kotlin.fir.builder.FirScriptConfiguratorExtension.Factory
 import org.jetbrains.kotlin.fir.declarations.FirDeclarationOrigin
 import org.jetbrains.kotlin.fir.declarations.builder.*
 import org.jetbrains.kotlin.fir.declarations.primaryConstructorIfAny
+import org.jetbrains.kotlin.fir.moduleData
 import org.jetbrains.kotlin.fir.resolve.providers.dependenciesSymbolProvider
 import org.jetbrains.kotlin.fir.symbols.SymbolInternals
 import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirValueParameterSymbol
 import org.jetbrains.kotlin.fir.types.builder.buildUserTypeRef
 import org.jetbrains.kotlin.fir.types.impl.FirQualifierPartImpl
 import org.jetbrains.kotlin.fir.types.impl.FirTypeArgumentListImpl
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.scripting.resolve.KtFileScriptSource
 import org.jetbrains.kotlin.scripting.resolve.VirtualFileScriptSource
-import kotlin.script.experimental.api.ScriptCompilationConfiguration
-import kotlin.script.experimental.api.SourceCode
-import kotlin.script.experimental.api.baseClass
+import kotlin.script.experimental.api.*
 import kotlin.script.experimental.host.FileScriptSource
 import kotlin.script.experimental.host.ScriptingHostConfiguration
 import kotlin.script.experimental.host.StringScriptSource
@@ -61,6 +62,31 @@ class FirScriptConfiguratorExtensionImpl(
                         valueParameters.add(buildValueParameterCopy(it) { origin = FirDeclarationOrigin.ScriptCustomization } )
                     }
                 }
+            }
+
+            definition.compilationConfiguration[ScriptCompilationConfiguration.implicitReceivers]?.forEach { implicitReceiver ->
+                contextReceivers.add(buildContextReceiverWithFqName(FqName.fromSegments(implicitReceiver.typeName.split("."))))
+            }
+
+            definition.compilationConfiguration[ScriptCompilationConfiguration.providedProperties]?.forEach { propertyName, propertyType ->
+                val typeRef = buildUserTypeRef {
+                    isMarkedNullable = propertyType.isNullable
+                    propertyType.typeName.split(".").forEach {
+                        qualifier.add(FirQualifierPartImpl(null, Name.identifier(it), FirTypeArgumentListImpl(null)))
+                    }
+                }
+                valueParameters.add(
+                    buildValueParameter {
+                        moduleData = session.moduleData
+                        origin = FirDeclarationOrigin.ScriptCustomization
+                        returnTypeRef = typeRef
+                        this.name = Name.identifier(propertyName)
+                        this.symbol = FirValueParameterSymbol(this.name)
+                        isCrossinline = false
+                        isNoinline = false
+                        isVararg = false
+                    }
+                )
             }
         }
     }
